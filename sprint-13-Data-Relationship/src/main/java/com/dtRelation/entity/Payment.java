d0a25eb7a9c2a96b8a689e708501fc374b49dfe4
ufuk058 <ufuksahinduran.uk@gmail.com>
2024-11-11 15:25:21 +0000

    Relation by foreign id(@OneToOne)

---
+++ sprint-13-Data-Relationship/src/main/java/com/dtRelation/entity/Payment.java
@@ -0,0 +1,42 @@
+package com.dtRelation.entity;
+
+import com.dtRelation.enums.Status;
+import jakarta.persistence.*;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.math.BigDecimal;
+import java.time.LocalDate;
+
+@Entity
+@NoArgsConstructor
+@Data
+@Table(name="payments")
+public class Payment {
+
+    public Payment(LocalDate createdDate, BigDecimal amount, Status paymentStatus) {
+        this.createdDate = createdDate;
+        this.amount = amount;
+        this.paymentStatus = paymentStatus;
+    }
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(columnDefinition = "DATE")
+    private LocalDate createdDate;
+
+    private BigDecimal amount;
+
+    @Enumerated(EnumType.STRING)
+    private Status paymentStatus;
+
+    @OneToOne //This annotation creates relation between Payment and PaymentDetail and accept id as foreign key
+    //and creates foreign id as payment_detail_id
+    // @JoinColumn(name="myForeignKey") //This annotation provides to custom name of the foreign key
+    private PaymentDetail paymentDetail;
+
+
+
+}
