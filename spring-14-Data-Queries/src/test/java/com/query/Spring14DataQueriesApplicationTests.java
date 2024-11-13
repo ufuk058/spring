5827ff0a58cb7307533769602d54893f3413ddfe
ufuk058 <ufuksahinduran.uk@gmail.com>
2024-11-13 14:44:24 +0000

    Data Queries modified

---
+++ spring-14-Data-Queries/src/test/java/com/query/Spring14DataQueriesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.query;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Spring14DataQueriesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
